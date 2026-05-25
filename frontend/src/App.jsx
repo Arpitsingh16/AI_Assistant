565451c5084e8f2b1a19dd99a4b16c49c62d8ebb
Arpit Singh <arpitchauhan1607@gmail.com>
2026-05-25 22:03:27 +0530

    fixed axios credentials

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
+import axios from 'axios'
+
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import Customize from './pages/Customize'
@@ -7,16 +9,62 @@ import { userDataContext } from './context/UserContext'
 import Home from './pages/Home'
 import Customize2 from './pages/Customize2'
 
+// IMPORTANT
+axios.defaults.withCredentials = true
+
 function App() {
-  const {userData,setUserData}=useContext(userDataContext)
+
+  const { userData, setUserData } = useContext(userDataContext)
+
   return (
-   <Routes>
-     <Route path='/' element={(userData?.assistantImage && userData?.assistantName)? <Home/> :<Navigate to={"/customize"}/>}/>
-    <Route path='/signup' element={!userData?<SignUp/>:<Navigate to={"/"}/>}/>
-     <Route path='/signin' element={!userData?<SignIn/>:<Navigate to={"/"}/>}/>
-      <Route path='/customize' element={userData?<Customize/>:<Navigate to={"/signup"}/>}/>
-       <Route path='/customize2' element={userData?<Customize2/>:<Navigate to={"/signup"}/>}/>
-   </Routes>
+    <Routes>
+
+      <Route
+        path='/'
+        element={
+          (userData?.assistantImage && userData?.assistantName)
+            ? <Home />
+            : <Navigate to={"/customize"} />
+        }
+      />
+
+      <Route
+        path='/signup'
+        element={
+          !userData
+            ? <SignUp />
+            : <Navigate to={"/"} />
+        }
+      />
+
+      <Route
+        path='/signin'
+        element={
+          !userData
+            ? <SignIn />
+            : <Navigate to={"/"} />
+        }
+      />
+
+      <Route
+        path='/customize'
+        element={
+          userData
+            ? <Customize />
+            : <Navigate to={"/signup"} />
+        }
+      />
+
+      <Route
+        path='/customize2'
+        element={
+          userData
+            ? <Customize2 />
+            : <Navigate to={"/signup"} />
+        }
+      />
+
+    </Routes>
   )
 }
 
